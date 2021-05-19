b2b1dccfb3799554155d9a0b28975a27b63450a7
Tomas Olivera <tomasnolivera@gmail.com>
2021-05-19 15:07:30 -0300

    Cambio links y el  ultimo icono del index

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -18,16 +18,3 @@ function mobile_icon_transformation() {
     line3.removeAttr("style");
   }
 }
-
-// propuestas.html 
-
-const CARD_TITLES = ["Esquelético", "Orgánico", "Muscular", "Energético", "Circulatorio", "Chakras", "Espiritual"];
-
-let cardDiv = $(".cards");
-
-for (cardTitle of CARD_TITLES) {
-  let card = `<div class="card">
-				  <div class="card-body">${cardTitle}</div>
-				</div>`;
-  cardDiv.append(card);
-}
