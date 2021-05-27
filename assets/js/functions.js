59b3ef7781f2e8c034dd48465d141bc1bc7cd9d8
Tomas Olivera <tomasnolivera@gmail.com>
2021-05-27 14:25:31 -0300

    Cambios para vista web

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,6 @@
 function show_header_content() {
   mobile_icon_transformation();
-  $("#mobile-header-content").toggle(1000);
+  $("#header-content").toggle(1000);
 }
 
 function mobile_icon_transformation() {
@@ -16,5 +16,22 @@ function mobile_icon_transformation() {
     line1.removeAttr("style");
     line2.removeAttr("style");
     line3.removeAttr("style");
+  }
+}
+
+function change_web_icon() {
+  let promotion_msj = document
+    .getElementById("categories-separation")
+    .getBoundingClientRect();
+
+  console.log(promotion_msj.top);
+  if (promotion_msj.top <= 190 && promotion_msj.top >= -325) {
+    console.log("Tiene que moverse");
+    $("#categories-separation").css({ "background-attachment": "fixed" });
+  } else {
+    console.log("No tiene que moverse");
+    console.log($("#categories-separation"));
+
+    $("#categories-separation").css({ "background-attachment": "local" });
   }
 }
