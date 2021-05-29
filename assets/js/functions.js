94b2af5b96c5106166d9909ba38c6c49ef8b3503
Tomas Olivera <tomasnolivera@gmail.com>
2021-05-29 00:16:14 -0300

    Arreglos vista web

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -18,22 +18,3 @@ function mobile_icon_transformation() {
     line3.removeAttr("style");
   }
 }
-
-function change_web_icon() {
-  let promotion_msj = document
-    .getElementById("categories-separation")
-    .getBoundingClientRect();
-  if (promotion_msj.top <= 210 && promotion_msj.top >= -300) {
-    $("#categories-separation").css({
-      "background-attachment": "fixed",
-      "background-position": "center",
-    });
-  } else {
-    $("#categories-separation").css({ "background-attachment": "local" });
-    if (promotion_msj.top >= 210) {
-      $("#categories-separation").css({ "background-position": "top" });
-    } else if (promotion_msj.top <= -300) {
-      $("#categories-separation").css({ "background-position": "bottom" });
-    }
-  }
-}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
 $(document).ready(function () {
-  window.addEventListener("scroll", change_web_icon);
   $("#mobile-icon").click(show_header_content);
 });
